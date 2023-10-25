945f2fae1f29562f7c111b1271826f07035143ea
Paul <paulwharin@gmail.com>
2023-10-25 16:47:32 +0100

    docs: add additional notes on refactoring exercise

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -17,8 +17,16 @@ function processTransactions(transactions) {
 module.exports = processTransactions;
 
 /*
--
--
--
--
+- Globals    Removing the assignment of an empty array to the global, txr, made the function pure.
+- Parameter  Renaming the parameter "transactions" describes the parameter's contents more accurately.
+- Validation Replacing the call to a validation function with a local IF statement gives fellow coders less to
+-            think about, and checking for an array prevents reduce() throwing an unknown function error at 
+-            runtime.
+- Reduce...  Implementing the transaction summary logic with a reduce() method clarified the code's intent.
+- Map...     Having a Map object as the reduce function's accumulator is cleaner than dynamically adding
+-            properties to an object.
+- Spread... A Map object is also iterable, providing a suitable input for the final two steps.
+- Sort... We've used .sort() to order the array firstly by quantity (decreasing), then alphabetically. 
+-         It makes the code more condensed as well as more readable, and gets rid of the seperate sort function.
+- Join... Finally, we've used .join() to correctly format the output from the map we used earlier
 */
